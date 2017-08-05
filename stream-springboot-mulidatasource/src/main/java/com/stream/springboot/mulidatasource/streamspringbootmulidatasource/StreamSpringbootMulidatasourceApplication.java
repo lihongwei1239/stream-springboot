8dd37ad88c6bd5cc3b892711bec207b6e7f41d8a
lihongwei1239@outlook.com <lihongwei1239@outlook.com>
2017-08-05 08:35:27 +0800

    lihongwei

---
+++ stream-springboot-mulidatasource/src/main/java/com/stream/springboot/mulidatasource/streamspringbootmulidatasource/StreamSpringbootMulidatasourceApplication.java
@@ -0,0 +1,12 @@
+package com.stream.springboot.mulidatasource.streamspringbootmulidatasource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamSpringbootMulidatasourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamSpringbootMulidatasourceApplication.class, args);
+	}
+}
---
+++ stream-springboot-mulidatasource/src/test/java/com/stream/springboot/mulidatasource/streamspringbootmulidatasource/StreamSpringbootMulidatasourceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.stream.springboot.mulidatasource.streamspringbootmulidatasource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StreamSpringbootMulidatasourceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
